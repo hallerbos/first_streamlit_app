8366aa7c2e196767d91055e9c16662a35369c496
Marek <115563763+hallerbos@users.noreply.github.com>
2022-10-11 16:10:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('This is really interesting')
 streamlit.header('The font should be smaller')
 streamlit.text('And this is how the normal text should look like')
-streamlit.text.bold('Will this work?')
+
